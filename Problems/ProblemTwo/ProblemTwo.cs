643276c72ab26aabf8ce6fe56d1dc736132ead50
plami <plamena.y.nikolova@gmail.com>
2015-05-19 22:56:53 +0300

    solutions

---
+++ Problems/ProblemTwo/ProblemTwo.cs
@@ -0,0 +1,15 @@
+﻿namespace ProblemTwo
+{
+    using System;
+
+    public class ProblemTwo
+    {
+        public static void Main()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(100 - i);
+            }
+        }
+    }
+}
